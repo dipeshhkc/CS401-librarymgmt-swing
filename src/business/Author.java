2c023e4f8373d5d623c8de5fc383978614bfd4a9
dipesh <kcdipesh429@gmail.com>
2022-12-09 10:45:54 -0600

    feat: add isExpert attribute in Author

--- src/business/Author.java
+++ src/business/Author.java
@@ -4,13 +4,20 @@ import java.io.Serializable;
 
 final public class Author extends Person implements Serializable {
 	private String bio;
+	private Boolean isExpert;
 	public String getBio() {
 		return bio;
 	}
 	
-	public Author(String f, String l, String t, Address a, String bio) {
+	public Boolean getIsExpert() {
+		return isExpert;
+	}
+	
+	
+	public Author(String f, String l, String t, Address a, String bio,Boolean isExpert) {
 		super(f, l, t, a);
 		this.bio = bio;
+		this.isExpert=isExpert;
 	}
 
 	private static final long serialVersionUID = 7508481940058530471L;
